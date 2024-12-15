0c4ba81223f02683a9213933dc1f20563e57081a
Keemss <kemardejesus@gmail.com>
2024-12-15 02:21:02 -0500

    Updated Version

---
+++ TippinAintEasy.py
@@ -0,0 +1,6 @@
+print("Tippin Aint Easy! Lets Get You On Your Way 😎.")
+bill = float(input("What was the total bill? $"))
+tip = int(input("What percentage tip would you like to give? 10 12 15 "))
+people = int(input("How many people to split the bill? "))
+
+print("")
---
+++ main.py
@@ -0,0 +1,8 @@
+print("Tippin Aint Easy! Lets Get You On Your Way 😎.")
+bill = float(input("What was the total bill? $"))
+tip = int(input("What percentage tip would you like to give? 10% 12% 15% ?"))
+people = int(input("How many people to split the bill? "))
+
+print(f"Each Person Should Pay: $ {round(bill * (tip / 100 + 1) / people , 2)}")
+goofy = input("Who Paid For It? (Insert Name")
+print(goofy + " You A Goofy 🤡🤡")
